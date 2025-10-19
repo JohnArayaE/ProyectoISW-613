4ec052d2907f312a293a9853cb0ad7d3dd0716dc
JohnArayaE <johnmai1205@gmail.com>
2025-10-18 21:26:10 -0600

    Se agrego la conexion a la base de datos

---
+++ common/conexion.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost"; 
+$user = "root"; 
+$pass = ""; 
+$db   = "proyecto1"; 
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+?>
